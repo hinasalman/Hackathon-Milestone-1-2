866d4dc2cd7c1750a7677a4ba2986f07920efc0e
Hina Salman <160792823+hinasalman@users.noreply.github.com>
2024-09-13 13:27:18 -0700

    Milestone 1,2

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-skills');
+const skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
